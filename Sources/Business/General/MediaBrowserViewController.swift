12a9a515c3095add27de73fe0ea127071e0e6c29
Jia Song <593908937@qq.com>
2025-01-22 17:41:40 +0800

    Revert "viewportInsets"
    This reverts commit 76d6432ea35d5ece33012014faca68835160b7a6.

--- Sources/Business/General/MediaBrowserViewController.swift
+++ Sources/Business/General/MediaBrowserViewController.swift
@@ -272,16 +272,18 @@ extension MediaBrowserViewController: MediaBrowserViewDataSource {
         if cell.zoomView == nil {
             cell.zoomView = self.configuration.zoomView(index)
         }
-        
-        cell.zoomView?.viewportInsets = { [weak self] in
-            guard let self = self else { return .zero }
+        guard let zoomView = cell.zoomView else {
+            assertionFailure()
+            return
+        }
+        zoomView.viewportInsets = {
             let insets = self.view.safeAreaInsets
             if JSCoreHelper.isMac {
                 return insets
             } else {
                 return UIEdgeInsets(top: 0, left: insets.left, bottom: 0, right: insets.right)
             }
-        }
+        }()
         
         let updateAsset = { [weak self] (cell: PhotoCell, asset: (any ZoomAsset)?, thumbnail: UIImage?) in
             guard let self = self else { return }
--- Sources/MediaView/Basis/BasisMediaView.swift
+++ Sources/MediaView/Basis/BasisMediaView.swift
@@ -10,7 +10,14 @@ import JSCoreKit
 
 open class BasisMediaView: UIView {
     
-    public var viewportInsets: (() -> UIEdgeInsets)?
+    public var viewportInsets: UIEdgeInsets = .zero {
+        didSet {
+            guard oldValue != self.viewportInsets else {
+                return
+            }
+            self.setNeedsLayout()
+        }
+    }
     
     /// 以下属性viewportRect为zero时才会生效, 若自定义viewportRect, 请自行实现
     private var viewportRectMaxWidth = 580.0
@@ -45,7 +52,7 @@ extension BasisMediaView {
         guard !self.bounds.isEmpty else {
             return CGRect.zero
         }
-        let viewportInsets = self.viewportInsets?() ?? .zero
+        let viewportInsets = self.viewportInsets
         let size = CGSize(width: min(self.bounds.width, self.viewportRectMaxWidth), height: self.bounds.height)
         let offsetX = (self.bounds.width - size.width) / 2
         let top = viewportInsets.top
