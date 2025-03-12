096a2439276e4c4a6f4a1a8b17226d3a08ef51f9
jiasong <593908937@qq.com>
2025-03-12 13:48:08 +0800

    fix

--- Example/JSMediaBrowserExample/Sources/MediaBrowser/Browser.swift
+++ Example/JSMediaBrowserExample/Sources/MediaBrowser/Browser.swift
@@ -10,73 +10,73 @@ import SDWebImage
 import JSMediaBrowser
 import PhotosUI
 
-struct ImageItem: PhotoAssetItem {
+struct ImageBuilder: PhotoAssetBuilder {
     
-    struct Builder: PhotoAssetBuilder {
-        
-        func createMediator() -> SDWebImagePhotosAssetMediator {
-            return .init(
-                options: [.retryFailed],
-                context: [
-                    .storeCacheType: SDImageCacheType.disk.rawValue,
-                    .queryCacheType: SDImageCacheType.disk.rawValue,
-                    .animatedImageClass: SDAnimatedImage.self
-                ]
-            )
-        }
-        
-        func createAssetView() -> ZoomView<SDAnimatedImageView> {
-            let assetView = SDAnimatedImageView()
-            assetView.autoPlayAnimatedImage = false
-            if #available(iOS 17.0, *) {
-                assetView.preferredImageDynamicRange = .high
-            }
-            let thumbnailView = SDAnimatedImageView()
-            thumbnailView.autoPlayAnimatedImage = false
-            return .init(
-                assetView: assetView,
-                thumbnailView: thumbnailView,
-                configuration: .init()
-            )
-        }
-        
+    func createMediator() -> SDWebImagePhotosAssetMediator {
+        return .init(
+            options: [.retryFailed],
+            context: [
+                .storeCacheType: SDImageCacheType.disk.rawValue,
+                .queryCacheType: SDImageCacheType.disk.rawValue,
+                .animatedImageClass: SDAnimatedImage.self
+            ]
+        )
     }
+    
+    func createAssetView() -> ZoomView<SDAnimatedImageView> {
+        let assetView = SDAnimatedImageView()
+        assetView.autoPlayAnimatedImage = false
+        if #available(iOS 17.0, *) {
+            assetView.preferredImageDynamicRange = .high
+        }
+        let thumbnailView = SDAnimatedImageView()
+        thumbnailView.autoPlayAnimatedImage = false
+        return .init(
+            assetView: assetView,
+            thumbnailView: thumbnailView,
+            configuration: .init()
+        )
+    }
+    
+}
+
+struct ImageItem: PhotoAssetItem {
     
     var source: SDWebImagePhotosAssetMediator.Source
     
     var thumbnail: UIImage?
     
-    var builder: Builder {
+    var builder: ImageBuilder {
         return .init()
+    }
+    
+}
+
+struct LivePhotoBuilder: PhotoAssetBuilder {
+    
+    func createMediator() -> PHLivePhotoMediator {
+        return .init()
+    }
+    
+    func createAssetView() -> ZoomView<PHLivePhotoView> {
+        let thumbnailView = SDAnimatedImageView()
+        thumbnailView.autoPlayAnimatedImage = false
+        return .init(
+            assetView: PHLivePhotoView(),
+            thumbnailView: thumbnailView,
+            configuration: .init()
+        )
     }
     
 }
 
 struct LivePhotoItem: PhotoAssetItem {
     
-    struct Builder: PhotoAssetBuilder {
-        
-        func createMediator() -> PHLivePhotoMediator {
-            return .init()
-        }
-        
-        func createAssetView() -> ZoomView<PHLivePhotoView> {
-            let thumbnailView = SDAnimatedImageView()
-            thumbnailView.autoPlayAnimatedImage = false
-            return .init(
-                assetView: PHLivePhotoView(),
-                thumbnailView: thumbnailView,
-                configuration: .init()
-            )
-        }
-        
-    }
-    
     var source: PHLivePhotoMediator.Source
     
     var thumbnail: UIImage?
     
-    var builder: Builder {
+    var builder: LivePhotoBuilder {
         return .init()
     }
     
